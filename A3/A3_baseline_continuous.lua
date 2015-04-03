7c9477b10fdbff168134ba34966f2fc2255ac21d
anselmrothe <anselmrothe@users.noreply.github.com>
2015-04-03 19:25:37 -0400

    delete temporal pooling layer (recommended!)

--- A3/A3_baseline_continuous.lua
+++ A3/A3_baseline_continuous.lua
@@ -166,17 +166,11 @@ function main()
 
     -- construct model:
     model = nn.Sequential()
-    
-    -- if you decide to just adapt the baseline code for part 2, you'll probably want to make this linear and remove pooling
-    model:add(nn.TemporalConvolution(1, 20, 10, 1))
-    
-    --------------------------------------------------------------------------------------
-    -- Replace this temporal max-pooling module with your log-exponential pooling module:
-    --------------------------------------------------------------------------------------
-    model:add(nn.TemporalMaxPooling(3, 1))
 
-    model:add(nn.Reshape(20*39, true))
-    model:add(nn.Linear(20*39, 1))
+    model:add(nn.Reshape(50, true))
+    model:add(nn.Linear(50, 10))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(10, 1))
 
     criterion = nn.MSECriterion()
     
