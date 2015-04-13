e463a22c123e54c1542c5b18b0f9f8afc5835418
dhalpern <David@172-17-63-37.DYNAPOOL.NYU.EDU>
2015-04-13 09:22:54 -0400

    Swap files

--- A3/Crepe/train/config.lua
+++ A3/Crepe/train/config.lua
@@ -8,12 +8,40 @@ require("nn")
 -- The namespace
 config = {}
 
-local alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+local file = io.open("cmudict-0.7b.symbols.txt")
+local alphabet = {}
+local dict = {}
+local a2 = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+for i = 1,#a2 do
+    table.insert(alphabet, a2:sub(i,i))
+    dict[a2:sub(i,i)] = i
+end
+i = #a2
+for line in file:lines() do
+    table.insert(alphabet, line)
+    dict[line] = i
+    i = i + 1
+end
+
+function tail(list)
+    return { select(3, unpack(list)) }
+end
+
+local file = io.open("cmudict-0.7c.txt")
+cmudict = {}
+for line in file:lines() do
+    local entry = line:split(" ")
+    local word = entry[1]
+    local phonemes = tail(entry)
+    cmudict[word] = phonemes
+end
 
 -- Training data
 config.train_data = {}
 config.train_data.file = "traindata.t7b"
 config.train_data.alphabet = alphabet
+config.train_data.dict = dict
+config.train_data.cmudict = cmudict
 config.train_data.length = 1014
 config.train_data.batch_size = 128
 
@@ -21,6 +49,8 @@ config.train_data.batch_size = 128
 config.val_data = {}
 config.val_data.file =  "valdata.t7b"
 config.val_data.alphabet = alphabet
+config.val_data.dict = dict
+config.train_data.cmudict = cmudict
 config.val_data.length = 1014
 config.train_data.batch_size = 128
 
